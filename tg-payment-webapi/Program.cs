c54d75ed96a55f9785b352ef45967c784dd8b5b4
Zenska11 <zenska96@gmx.de>
2023-11-21 16:19:23 +0100

    added Swagger

--- tg-payment-webapi/Program.cs
+++ tg-payment-webapi/Program.cs
@@ -36,9 +36,17 @@ builder.Services.AddDbContext<PaymentDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 app.UseCors(ProverbAllowSpecificOrigins);
