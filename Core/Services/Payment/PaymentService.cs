db3cd2c51dc3a9f8df0c89c5f7a735c30cc6707e
Zenska11 <zenska96@gmx.de>
2023-11-25 22:48:32 +0100

    produces a Kafka message after a payment has been updated

--- Core/Services/Payment/PaymentService.cs
+++ Core/Services/Payment/PaymentService.cs
@@ -1,14 +1,22 @@
-﻿public class PaymentService : IPaymentService
+﻿using Confluent.Kafka;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Net;
+
+public class PaymentService : IPaymentService
 {
     private readonly ILogger<PaymentService> _logger;
     private readonly IPaymentRepository _paymentRepository;
+    private readonly IConfiguration _configuration;
 
-    public PaymentService(ILogger<PaymentService> logger, IPaymentRepository paymentRepository)
+    public PaymentService(ILogger<PaymentService> logger, IPaymentRepository paymentRepository, IConfiguration configuration)
     {
         _logger = logger;
         _paymentRepository = paymentRepository;
-
+        _configuration = configuration;
     }
+
+
 
     public async Task<List<PaymentDto>?> GetPayments()
     {
@@ -41,7 +49,13 @@
 
     public async Task UpdatePayment(PaymentUpdateDto paymentUpdateDto)
     {
+        // get topic from appsettings.json
+        var kafka_topic = _configuration.GetSection("Kafka").GetSection("Topic1").Value;
+        var kafka_broker = _configuration.GetSection("Kafka").GetSection("Broker").Value;
+        _logger.LogInformation($" Topic: {kafka_topic}");
+
         var payment = await _paymentRepository.GetPayment((Guid)paymentUpdateDto.UserId);
+        
 
         if (payment == null)
         {
@@ -53,6 +67,30 @@
         payment.Username = paymentUpdateDto.Username;
 
         await _paymentRepository.UpdatPayment(payment);
+
+        // Produce messages
+        ProducerConfig configProducer = new ProducerConfig
+        {
+            BootstrapServers = kafka_broker,
+            ClientId = Dns.GetHostName()
+        };
+
+        var demoPayment = new PaymentDto
+        {
+            Firstname = paymentUpdateDto.Firstname,
+            Lastname = paymentUpdateDto.Lastname,
+            Username = paymentUpdateDto.Username
+        };
+
+        using var producer = new ProducerBuilder<Null, string>(configProducer).Build();
+        
+        var result = await producer.ProduceAsync(kafka_topic, new Message<Null, string>
+        {
+            Value = JsonSerializer.Serialize<PaymentDto>(demoPayment)
+        });
+        Console.WriteLine(JsonSerializer.Serialize<PaymentDto>(demoPayment));
+
+        
     }
 }
 
